b8f56700a7985493cf1b515918b98c7799fb8057
eddyizm <eddyizm@gmail.com>
2024-02-10 18:03:24 -0800

    wrapped load file to try except

--- launch.py
+++ launch.py
@@ -88,11 +88,15 @@ def download_models():
     for file_name, url in vae_approx_filenames:
         load_file_from_url(url=url, model_dir=config.path_vae_approx, file_name=file_name)
 
-    load_file_from_url(
-        url='https://huggingface.co/lllyasviel/misc/resolve/main/fooocus_expansion.bin',
-        model_dir=config.path_fooocus_expansion,
-        file_name='pytorch_model.bin'
-    )
+    try: 
+        load_file_from_url(
+            url='https://huggingface.co/lllyasviel/misc/resolve/main/fooocus_expansion.bin',
+            model_dir=config.path_fooocus_expansion,
+            file_name='pytorch_model.bin'
+        )
+    except Exception as ex:
+        print('download failed. make do without')
+
 
     if args.disable_preset_download:
         print('Skipped model download.')
